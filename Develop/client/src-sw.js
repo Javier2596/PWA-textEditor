f704e97b1734a4ab0ea68211557aeb3d2678ecbc
Chito <Javier.martinez632@gmail.com>
2023-02-12 15:28:28 -0700

    updated install.js

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,18 +27,8 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+offlineFallback();
+
 registerRoute(
-  ({ request }) => request.destination === 'image',
-  new CacheFirst({
-    cacheName: 'my-image-cache',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-      }),
-    ],
-  })
+ 
 );
